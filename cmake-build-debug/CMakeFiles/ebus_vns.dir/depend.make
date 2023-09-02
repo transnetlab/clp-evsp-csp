cc0087753414dbe0a59e6fcc2fb7c8835c40ac16
Tarun Rambha <tarunrambha@iisc.ac.in>
2023-09-02 09:06:57 +0530

    Upload only relevant files

--- cmake-build-debug/CMakeFiles/ebus_vns.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ebus_vns.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/ebus_vns.dir/operators.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/ebus_vns.dir/operators.cpp.o: \
- /mnt/c/Users/Tarun/CLionProjects/ebus-vns/operators.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/Tarun/CLionProjects/ebus-vns/operators.h
